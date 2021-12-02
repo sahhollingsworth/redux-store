02019492a13d1c1b3040e3b99298f73d54f68945
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-12-01 21:48:59 -0800

    Implemented Redux state management in the ProductItem component

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+//import Dispatch & Selector hooks from redux
+import { useDispatch, useSelector } from 'react-redux';
+
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  //  Returns a reference to the dispatch function from the Redux store. Used to dispatch actions as needed.
+  const dispatch = useDispatch();
+
+  // Runs whenever an action (resolver) is dispatched and determines the (new) state value as a result of that action 
+  const state = useSelector((state) => state);
 
   const {
     image,
