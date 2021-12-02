a787364daa1e8336c25868b42a078d140b162f9d
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-12-01 20:45:36 -0800

    Replaced Global State usage with Redux state managemenget in top level app js

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,17 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+// Import component that allows store access to all children components
+import { Provider } from 'react-redux';
+// Import our custom store context instance
+import store from '/utils/store';
+
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
@@ -39,21 +43,22 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={ client }>
       <Router>
         <div>
-          <StoreProvider>
+        {/* Import the Provider to make the Redux store available to any nested components that need to access our Redux store. */}
+          <Provider store={ store }>
             <Nav />
             <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/success" component={Success} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
+              <Route exact path="/" component={ Home } />
+              <Route exact path="/login" component={ Login } />
+              <Route exact path="/signup" component={ Signup } />
+              <Route exact path="/success" component={ Success } />
+              <Route exact path="/orderHistory" component={ OrderHistory } />
+              <Route exact path="/products/:id" component={ Detail } />
+              <Route component={ NoMatch } />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
