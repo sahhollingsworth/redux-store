380684490cfa78166327ae089a0453cfbd64f3ff
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-12-01 22:01:59 -0800

    Implemented Redux state management in the CartItem component

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
+//import Dispatch hook from redux
+import { useDispatch } from 'react-redux';
+
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
-
-  const [, dispatch] = useStoreContext();
+  //  Returns a reference to the dispatch function from the Redux store. Used to dispatch actions as needed.
+  const dispatch = useDispatch();
 
   const removeFromCart = item => {
     dispatch({
@@ -45,14 +47,14 @@ const CartItem = ({ item }) => {
         />
       </div>
       <div>
-        <div>{item.name}, ${item.price}</div>
+        <div>{ item.name }, ${item.price}</div>
         <div>
           <span>Qty:</span>
           <input
             type="number"
             placeholder="1"
-            value={item.purchaseQuantity}
-            onChange={onChange}
+            value={ item.purchaseQuantity }
+            onChange={ onChange }
           />
           <span
             role="img"
